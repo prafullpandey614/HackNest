d7b283e3f0d77d9a9d8eb5325217e1396c7c2842
prafullpandey614 <prafullpandey614@gmail.com>
2024-01-27 00:54:39 +0530

    setup

--- HackNest/settings.py
+++ HackNest/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = str(os.getenv('SECRET_KEY'))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["env-hackenv.eba-nygpup6f.ap-south-1.elasticbeanstalk.com"]
+ALLOWED_HOSTS = ["env-hackenv.eba-nygpup6f.ap-south-1.elasticbeanstalk.com","*"]
 
 
 # Application definition
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -125,3 +126,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / "media"
--- app/urls.py
+++ app/urls.py
@@ -6,12 +6,15 @@ from django.conf.urls.static import static
 
 
 urlpatterns = [
-     path('home/',Home.as_view(),name='home'),
+     path('',Home.as_view(),name='home'),
      path('register/',Register.as_view(),name='register'),
      path('login/',Login.as_view(),name='login'),
      path('logout/',Logout.as_view(),name='logout'),
-     path('hackathons/',Hackathons.as_view(),name='hackathons'),
+     path('hackathons-list/',Hackathonss.as_view(),name='hackathons'),
+     path('hackathons/',HackathonsList.as_view(),name='hackathons'),
      path('single-hackathon-page/',SingleHackathonView.as_view(),name='singlehackathon'),
      path('profile/',Profile.as_view(),name='profile'),
+     path('add-hackathon/',AddHackathonAPIView.as_view(),name='prosile'),
 
 ] + static(settings.STATIC_URL , document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
